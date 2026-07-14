01a628e10d0a7deaf7de06c9def6facb2544389b
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-07-14 23:17:27 +0500

    fixed model positions for mobile

--- portfolio/src/utils/CalculatrSizes.jsx
+++ portfolio/src/utils/CalculatrSizes.jsx
@@ -21,14 +21,14 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
       ? [0, -Math.PI / 12, 0]
       : [0, -0.2, 0],
     targetPositions: isSmall
-      ? [0, -2.8, 0]
+      ? [0, -3.5, 0]
       : isMobile
       ? [0.6, 0.2, 0]
       : isTablet
       ? [1, 0.5, 1]
       : [0.6, 0.2, 0],
     targetScales: isSmall
-      ? 0.5
+      ? 1
       : isMobile
       ? 1.1
       : isTablet
